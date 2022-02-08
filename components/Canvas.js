a9b592d3c60cb1f0c96a4e1fb7ccb73176fc1fef
jamesgrubb <22652+jamesgrubb@users.noreply.github.com>
2022-02-08 22:37:50 +0000

    flashing shapes

--- components/Canvas.js
+++ components/Canvas.js
@@ -1,12 +1,17 @@
 import { Engine, Render, World, Bodies } from 'matter-js';
 import { useEffect, useRef } from 'react';
 import { paper } from 'paper';
+
+const random = (min, max) => {
+	return Math.random() * min + Math.random() * max + min;
+};
+
 function Canvas(props) {
 	const scene = useRef();
 	const engine = useRef(Engine.create());
 	const paperRef = useRef();
 	const raf = useRef();
-
+	console.log(random);
 	useEffect(() => {
 		const time = 0;
 		window.addEventListener('load', () => {
@@ -46,7 +51,7 @@ function Canvas(props) {
 			Engine.run(engine.current);
 			Render.run(render);
 			const balls = [];
-			for (let i = 0; i < 20; i++) {
+			for (let i = 0; i < 40; i++) {
 				balls.push(
 					Bodies.circle(Math.random() * cw, 0, 80, {
 						density: Math.random(),
@@ -76,22 +81,54 @@ function Canvas(props) {
 			for (let i = 0; i < balls.length; i++) {
 				World.add(engine.current.world, [balls[i]]);
 			}
-
-			console.log(ball);
-			const callback = () => {
-				paper.view.update();
-				paper.project.clear();
-				balls.forEach((ball) => {
-					let shape = new paper.Path.RegularPolygon({
+			const shapes = [];
+			for (let i = 0; i < balls.length; i++) {
+				shapes.push(
+					new paper.Path.RegularPolygon({
 						position: new paper.Point([
-							ball.position.x,
-							ball.position.y,
+							balls[i].position.x,
+							balls[i].position.y,
 						]),
-						sides: 5,
+						sides: Math.floor(random(0, 8)),
+						radius: 100,
+						fillColor: 'tomato',
+					})
+				);
+			}
+			class Shape {
+				constructor(x, y, angle) {
+					this.angle = angle;
+					this.x = x;
+					this.y = y;
+					this.draw();
+					this.shape = new paper.Path.RegularPolygon({
+						position: new paper.Point([this.x, this.y]),
+						sides: Math.floor(random(0, 8)),
 						radius: 100,
 						fillColor: 'tomato',
 					});
-					shape.rotate(ball.angle);
+				}
+				draw() {
+					// this.shape.rotate(this.angle);
+				}
+			}
+			console.log(ball);
+			const callback = () => {
+				// paper.view.update();
+				paper.project.clear();
+
+				balls.forEach((ball) => {
+					new Shape(ball.position.x, ball.position.y, ball.angle);
+					// let shape = new paper.Path.RegularPolygon({
+					// 	position: new paper.Point([
+					// 		ball.position.x,
+					// 		ball.position.y,
+					// 	]),
+					// 	sides: Math.floor(Math.random() * 8),
+					// 	radius: 100,
+					// 	fillColor: 'tomato',
+					// });
+					// shape.rotate(ball.angle);
 				});
 				const shape = new paper.Path.RegularPolygon({
 					position: new paper.Point([
